18ce7dace93e71e98e98c760a386a798d208518d
Madison Randle <madisonrandle25@gmail.com>
2020-05-05 22:52:58 -0500

    Add article propertis to display full card

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,7 +3,12 @@ import './NewsArticle.css';
 
 const NewsArticle = (article) => {
   return (
-    
+    <section>
+      <img src={ article.article.img } alt="descriptive picture of article"/>
+      <h2>{ article.article.headline }</h2>
+      <p>{ article.article.description }</p>
+      <a href={ article.article.url }>Read Full Article</a>
+    </section>
   )
 }
 
