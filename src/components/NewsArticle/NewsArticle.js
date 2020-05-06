6821e8242fc284e9d02d6c48e13c1318f657c7c0
Madison Randle <madisonrandle25@gmail.com>
2020-05-06 17:47:22 -0500

    Update dot notation after destructuring NewsContainer

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -4,10 +4,10 @@ import './NewsArticle.css';
 const NewsArticle = (article) => {
   return (
     <section>
-      <img src={ article.article.img } alt="descriptive picture of article"/>
-      <h2>{ article.article.headline }</h2>
-      <p>{ article.article.description }</p>
-      <a href={ article.article.url }>Read Full Article</a>
+      <img src={ article.img } alt="descriptive picture of article"/>
+      <h2>{ article.headline }</h2>
+      <p>{ article.description }</p>
+      <a href={ article.url }>Read Full Article</a>
     </section>
   )
 }
