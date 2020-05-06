3c5e9499335b4e78cbc636a09b050add8b880e3d
Madison Randle <madisonrandle25@gmail.com>
2020-05-05 22:25:27 -0500

    Create NewsArticle skeleton

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './NewsArticle.css';
 
-const NewsArticle = (props) => {
-  
+const NewsArticle = (article) => {
+  return (
+    
+  )
 }
 
 export default NewsArticle;
