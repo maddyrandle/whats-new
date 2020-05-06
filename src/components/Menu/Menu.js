77df0b1ef1b1e5f5559fb20f82f7f87d0741f837
Madison Randle <madisonrandle25@gmail.com>
2020-05-06 17:46:11 -0500

    Add name to all buttons that calls getPageName onClick

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = (props) => {
+  const getPageName = (e) => {
+    props.updateState(e.target.name)
+  }
+
+  return (
+    <nav className="menu">
+      <button name="local" onClick={ getPageName }>
+        Local
+      </button>
+      <button name="entertainment" onClick={ getPageName }>
+        Entertainment
+      </button>
+      <button name="health" onClick={ getPageName }>
+        Health
+      </button>
+      <button name="science" onClick={ getPageName }>
+        Science
+      </button>
+      <button name="technology" onClick={ getPageName }>
+        Technology
+      </button>
+    </nav>
+  )
+}
 
 export default Menu;
