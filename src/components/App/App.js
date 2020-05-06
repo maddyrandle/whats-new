363bf18e26ee450986d3e26f59f1e3f0b3154e07
Madison Randle <madisonrandle25@gmail.com>
2020-05-06 17:45:25 -0500

    Add updateState method and a currentPage property for initial state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import NewsContainer from '../NewsContainer/NewsContainer'
+import Menu from '../Menu/Menu';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology,
+      currentPage: local
     }
+  }
+
+  updateState = (page) => {
+    this.setState({ currentPage: this.state[page] });
   }
 
   render () {
     return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-        <NewsContainer local={ this.state.local }/>
-        // passing down my state as props with "local"
-      </div>
+      <main className="app">
+        <Menu updateState={ this.updateState }/>
+        <section>
+          <NewsContainer articles={ this.state.currentPage }/>
+        </section>
+      </main>
     );
   }
 }
