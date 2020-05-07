977a8b617909e53df324a0084769cd91bd65d9af
Madison Randle <madisonrandle25@gmail.com>
2020-05-07 15:13:07 -0500

    Write method to update state to search input value

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      searchInput: ''
+    }
+  }
+
+  setSearchInput = (e) => {
+    this.setState({ searchInput: e.target.value })
+    console.log('DUUUU', this.state);
+  }
+
+  render () {
+    return (
+      <form className="search-form">
+        <input
+        className="search-input"
+        type="text"
+        name="searchInput"
+        placeholder="search news articles"
+        onChange={ (e) => this.setSearchInput(e) }
+        />
+
+        <button className="search-button">search</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
