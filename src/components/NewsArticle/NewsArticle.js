eb21538ce12bddafab208d294e5252b3542b6095
Madison Randle <madisonrandle25@gmail.com>
2020-05-06 23:02:52 -0500

    Update JSX for styling

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,8 +3,8 @@ import './NewsArticle.css';
 
 const NewsArticle = (article) => {
   return (
-    <section>
-      <img src={ article.img } alt="descriptive picture of article"/>
+    <section className="news-article">
+      <img src={ article.img } alt=""/>
       <h2>{ article.headline }</h2>
       <p>{ article.description }</p>
       <a href={ article.url }>Read Full Article</a>
